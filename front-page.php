d61d569bbf77c69df9de0b75c81ea19f77f71385
Patrick Fung <patrick@makestuffdostuff.com>
2015-11-16 12:29:12 -0500

    beginnings of forming a frontpage template for the library site

---
+++ front-page.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for the homepage
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package underscores
+ */
+
+get_header(); ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main clear" role="main">
+
+      <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(array('content-section', 'blurb')); ?>>
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div><!-- .entry-content -->
+
+          <footer class="entry-footer">
+            <?php edit_post_link( esc_html__( 'Edit', 'underscores' ), '<span class="edit-link">', '</span>' ); ?>
+          </footer><!-- .entry-footer -->
+        </article><!-- #post-## -->
+
+      <?php endwhile; // End of the loop. ?>
+
+    </main><!-- #main -->    
+    
+  </div><!-- #primary -->
+  
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+  if ( is_front_page() ) {
+    wp_enqueue_style( 'frontpage-style', get_stylesheet_directory_uri() . '/css/frontpage.css' );
+    wp_enqueue_style( 'summon-style', get_stylesheet_directory_uri() . '/css/summon.css' );
+  }
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
