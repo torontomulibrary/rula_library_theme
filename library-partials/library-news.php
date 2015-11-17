bc4d86c7d44c641ce0ad29d2f283cc4748e38614
Patrick Fung <patrick@makestuffdostuff.com>
2015-11-17 11:41:26 -0500

    fix newsfeed styles

--- library-partials/library-news.php
+++ library-partials/library-news.php
@@ -1,12 +1,17 @@
-<h2 class="grey-gradient">
-  <a href="http://library.ryerson.ca/blog/" style="text-decoration: none">Library News</a>
-  <a href="http://apps.library.ryerson.ca/api/merge/"  data-ga-event="Homepage,SocialMedia,RSS">
-    <img class="wp-image-x noborder" src="/wp-content/uploads/rss.gif" alt="Subscribe to news feed" width="14" height="12" />
-  </a>
-</h2>
-
 <!--Library News Feed-->
-<script language="JavaScript" src="http://apps.library.ryerson.ca/feed2js/feed2js.php?src=http://apps.library.ryerson.ca/api/merge/&num=5&utf=y"  charset="UTF-8" type="text/javascript"></script>
-<noscript>
-  <a href="http://apps.library.ryerson.ca/feed2js/feed2js.php?src=http://apps.library.ryerson.ca/api/merge/&num=5&utf=y&html=y">View RSS feed</a>
-</noscript>
+
+<aside class="card libnews">
+  <header class="card-header">
+    <h2 class="grey-gradient">
+      <a href="http://library.ryerson.ca/blog/">Library News</a>
+      <a href="http://apps.library.ryerson.ca/api/merge/" data-ga-event="Homepage,SocialMedia,RSS">
+        <img class="rss-icon noborder" src="<?php bloginfo('template_directory'); ?>/img/social-icons/32x32%20PNG/32-rss.png" alt="Subscribe to news feed">
+      </a>
+    </h2>
+  </header>
+
+  <script language="JavaScript" src="http://apps.library.ryerson.ca/feed2js/feed2js.php?src=http://apps.library.ryerson.ca/api/merge/&num=5&utf=y"  charset="UTF-8" type="text/javascript"></script>
+  <noscript>
+    <a href="http://apps.library.ryerson.ca/feed2js/feed2js.php?src=http://apps.library.ryerson.ca/api/merge/&num=5&utf=y&html=y">View RSS feed</a>
+  </noscript>
+</aside>
