e1736faa083581f3c7f2ce34cc7cc9d388be9bb7
Patrick Fung <patrick@makestuffdostuff.com>
2015-11-24 13:56:26 -0500

    split out css specific to the rula_tabs plugin

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 function theme_enqueue_styles() {
   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  wp_register_style('rula-tabs-style', get_stylesheet_directory_uri() . '/css/rula_tabs.css');
 
   wp_register_script('rula-tabs-plugin', get_stylesheet_directory_uri() . '/js/rula_tabs.js', array('jquery'), '20151124', true );
 
   if ( is_front_page() ) {
-    wp_enqueue_style( 'frontpage-style', get_stylesheet_directory_uri() . '/css/frontpage.css' );
-    wp_enqueue_style( 'summon-style', get_stylesheet_directory_uri() . '/css/summon.css' );
+    wp_enqueue_style( 'frontpage-style', get_stylesheet_directory_uri() . '/css/front-page.css', array('rula-tabs-style') );
     wp_enqueue_script( 'frontpage-script', get_stylesheet_directory_uri() . '/js/front-page.js', array('rula-tabs-plugin'), '20151124', true );
   }
 }
