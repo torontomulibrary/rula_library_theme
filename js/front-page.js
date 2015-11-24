1afba9693e6784d7656d5bbb9fe07b5c510fe86e
Patrick Fung <patrick@makestuffdostuff.com>
2015-11-24 11:43:15 -0500

    wrap rula_tabs plugin with some better jquery standards

--- js/front-page.js
+++ js/front-page.js
@@ -1,48 +1,82 @@
-$.fn.rula_tabs = function() {
-  // Initialize plugin
-  var self = this;
+;(function ( $, window, document, undefined ) {
 
-  // Insert container for tab
-  self.prepend('<ul class="tab-list"></ul>');
-  var tabList = self.children('.tab-list').first();
+  "use strict";
 
-  // Insert container for displaying tab content
-  self.append('<div class="tab-display"></div>');
-  var tabDisplay = self.children('.tab-display').first();
+  var pluginName = "rula_tabs",
+  defaults = {
+    propertyName: "value"
+  };
 
-  // Generate Tabs
-  $.each(self.children('.tab'), generateTabs);
+  // The actual plugin constructor
+  function Plugin ( element, options ) {
+    this.element = element;
+    // jQuery has an extend method which merges the contents of two or
+    // more objects, storing the result in the first object. The first object
+    // is generally empty as we don't want to alter the default options for
+    // future instances of the plugin
+    this.settings = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
 
-  // Set the first tab to be active
-  tabList.children().first().addClass('active');
+    this.tabList = undefined;
+    this.tabContent = undefined;
 
-  // Insert content of first tab into the display
-  updateTabDisplay( self.children('.tab').first() );
-
-  // Bind onClick event to tabs
-  tabList.children('li').click(onTabClicked);
-
-  // Function to generate tabs
-  function generateTabs(index, value) {
-    tabList.append('<li data-tab-index="'+ index +'">' + $(value).data('tab-title') + '</li>');
+    this.init();
+    this.generateTabs();
+    this.displayTabContent(0);
   }
 
-  // Callback handler when tabs are clicked 
-  function onTabClicked() {
-    var tabIndex = $(this).data('tab-index');
-    
-    // Set the active tab
-    tabList.children('.active').first().removeClass('active');
-    $(this).addClass('active');
+  // Avoid Plugin.prototype conflicts
+  $.extend(Plugin.prototype, {
+    init: function() {
+      // Grab the list of tabs
+      this.tabs = $(this.element).children('.tab');
 
-    updateTabDisplay( self.children('.tab').eq(tabIndex) );
-  }
-  // Update the tab display element
-  function updateTabDisplay(element) {
-    tabDisplay.empty();
-    tabDisplay.append( element.html() );
-  }
-};
+      // Prepend the container for tabs
+      this.tabList = $('<ul class="tab-list"></ul>').prependTo(this.element);
+
+      // Append the container for displaying tab content
+      this.tabContent = $('<div class="tab-content"></div>').appendTo(this.element);
+    },
+    generateTabs: function() {
+      var plugin_context = this;
+      var $tabs = this.tabs;
+      var $tabList = this.tabList; // there's gotta be a better way than this...
+
+      // For each <div class="tab"> add it to the tabList, and bind an event handler to it.
+      $tabs.each(function(index, value) {
+        var $tab = $('<li data-tab-index="'+ index +'">' + $(value).data('tab-title') + '</li>').appendTo($tabList);
+
+        $tab.click(function() { // there's gotta be a better way than this...
+          plugin_context.displayTabContent($(this).data('tab-index')); 
+        });
+      });
+    },
+    displayTabContent: function(tabIndex) {
+      var $tabContent = this.tabContent,
+      $tabList = this.tabList,
+      $newActiveTabContent = this.tabs.eq(tabIndex).html(); // there's gotta be a better way than this...
+
+      // Set the active tab class
+      $tabList.children('.active').removeClass('active');
+      $tabList.children().eq(tabIndex).addClass('active');
+
+      // insert content of tab into display
+      $tabContent.empty().append( $newActiveTabContent );
+    }
+  });
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[ pluginName ] = function ( options ) {
+    return this.each(function() {
+      if ( !$.data( this, "plugin_" + pluginName ) ) {
+        $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+      }
+    });
+  };
+
+})( jQuery, window, document );
 
 $(function() {
   $('.tabset').rula_tabs();
